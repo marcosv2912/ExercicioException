8896ef2edf4785732b609a20ebc6360066f57970
Marcos Paixao <marcosv2912@gmail.com>
2019-10-22 08:40:12 -0300

    primeiro commit

---
+++ ExercicioException/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioException.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string messege) : base(messege)
+        {
+
+        }
+    }
+}
---
+++ ExercicioException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ExercicioException.Entities;
+using ExercicioException.Entities.Exceptions;
+using System.Globalization;
+
+namespace ExercicioException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdraw);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                account.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+                Console.WriteLine("New Balance: " + account);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
